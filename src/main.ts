d0ce35ac6e9e2976e5ce3bc798b0c237cce7b61c
Ebiu Julius <ebiujchris@gmail.com>
2026-06-01 14:30:19 +0300

    report issue enable and user role info leakage fix

--- src/main.ts
+++ src/main.ts
@@ -11,9 +11,12 @@ async function bootstrap() {
       'http://localhost:3000',
       'http://localhost:3001',
       'http://localhost:3002',
+      'https://smartrent-fe-blush.vercel.app',
       process.env.FRONTEND_URL,
     ].filter(Boolean),
     credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   });
   
   app.useGlobalPipes(
